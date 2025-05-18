0a83789cc1ada332c19700f61767573dcdc55b79
elkprostoelk <novikovmisha138138@gmail.com>
2025-05-18 21:32:03 +0300

    added loader for a profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {Card} from 'primeng/card';
 import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
-import {mergeMap} from 'rxjs';
+import {mergeMap, take, tap} from 'rxjs';
 import {DatePipe, NgIf, NgOptimizedImage} from '@angular/common';
 import {UserProfileService} from '../../services/user-profile.service';
 import {UserProfileDto} from '../../dto/userProfileDto';
 import {MessageService} from 'primeng/api';
+import {ProgressSpinner} from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-profile',
@@ -14,13 +15,15 @@ import {MessageService} from 'primeng/api';
     Card,
     NgOptimizedImage,
     NgIf,
-    DatePipe
+    DatePipe,
+    ProgressSpinner
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.less'
 })
 export class ProfileComponent implements OnInit {
   userProfile: UserProfileDto = new UserProfileDto();
+  isProfileLoading = false;
 
   constructor(
     private readonly router: Router,
@@ -30,19 +33,21 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
     this.authService.loggedInUserData$.pipe(
+      take(1),
+      tap(() => this.isProfileLoading = true),
       mergeMap(userData => this.userProfileService.getUserProfile(userData!.userId)))
       .subscribe({
-        next: (userProfile) => {
-          this.userProfile = userProfile;
-        },
+        next: (userProfile) => this.userProfile = userProfile,
         error: () => {
           this.messageService.add({
             severity: 'error',
             summary: 'Error!',
             data: 'Failed to get the user profile!',
             life: 3000
-          })
-        }
+          });
+          this.isProfileLoading = false;
+        },
+        complete: () => this.isProfileLoading = false
       });
   }
 }
